0a41a6503345c129a4ec858238418d1683b38fad
Artur Galimsky <artur.galimsky@gmail.com>
2021-03-20 19:51:38 +0500

    doubleClick open taskeditwindow

--- Galimsky DayPlanner/TasksControl.xaml.cs
+++ Galimsky DayPlanner/TasksControl.xaml.cs
@@ -37,17 +37,24 @@ namespace Galimsky_DayPlanner
         {
             if (_repo.DayTaskListSelection != null)
             {
-                TaskEditWindow taskEditorWindow = new TaskEditWindow();
-                taskEditorWindow.Owner = Application.Current.MainWindow;
-                taskEditorWindow.Configure(TaskEditorMode.Edit);
-                taskEditorWindow.ShowDialog();
+                OpenTaskEditWindow(TaskEditorMode.Edit);
             }
         }
         private void Tasks_AddItem_Click(object sender, RoutedEventArgs e)
         {
+            OpenTaskEditWindow(TaskEditorMode.New);
+        }
+
+        private void DayTasksList_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            OpenTaskEditWindow(TaskEditorMode.Edit);
+        }
+
+        private void OpenTaskEditWindow(TaskEditorMode mode)
+        {
             TaskEditWindow taskEditorWindow = new TaskEditWindow();
             taskEditorWindow.Owner = Application.Current.MainWindow;
-            taskEditorWindow.Configure(TaskEditorMode.New);
+            taskEditorWindow.Configure(mode);
             taskEditorWindow.ShowDialog();
         }
     }
