e65bc4a89ed49a5bf9bdd212ff33e7b32667363d
Artur Galimsky <artur.galimsky@gmail.com>
2021-03-20 15:21:35 +0500

    PhonesWindow to PhonesControl redesign.

---
+++ Galimsky DayPlanner/PhonesControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Galimsky_DayPlanner
+{
+    /// <summary>
+    /// Interaction logic for PhonesControl.xaml
+    /// </summary>
+    public partial class PhonesControl : UserControl
+    {
+        public PhonesControl()
+        {
+            InitializeComponent();
+            DataContext = PhonesRepo.Instance;
+        }
+
+        private void PhonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+                PhonesRepo.Instance.SelectedPhone = e.AddedItems[0] as PhoneData;
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            PhonesRepo.Instance.Phones.Add(PhoneData.Create());
+        }
+
+        private void ButtonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            PhonesRepo.Instance.ItemsView.Refresh();
+        }
+    }
+}
--- Galimsky DayPlanner/PhonesWindow.xaml.cs
+++ Galimsky DayPlanner/PhonesWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Galimsky_DayPlanner
 {
@@ -19,28 +7,9 @@ namespace Galimsky_DayPlanner
     /// </summary>
     public partial class PhonesWindow : Window
     {
-        //#endregion
-
         public PhonesWindow()
         {
             InitializeComponent();
-            DataContext = PhonesRepo.Instance;
-        }
-
-        private void PhonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if(e.AddedItems.Count>0)
-                PhonesRepo.Instance.SelectedPhone = e.AddedItems[0] as PhoneData;   
-        }
-
-        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
-        {
-            PhonesRepo.Instance.Phones.Add(PhoneData.Create());
-        }
-
-        private void ButtonRefresh_Click(object sender, RoutedEventArgs e)
-        {
-            PhonesRepo.Instance.ItemsView.Refresh();
         }
     }
 }
