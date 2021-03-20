78ea988e31ac9ec287aae47cfb79a9a0532b62da
Artur Galimsky <artur.galimsky@gmail.com>
2021-03-20 15:30:37 +0500

    Tasks layout to separate TasksControl file

--- Galimsky DayPlanner/MainWindow.xaml.cs
+++ Galimsky DayPlanner/MainWindow.xaml.cs
@@ -1,22 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.ComponentModel;
-using System.Xml.Linq;
-using System.IO;
+﻿using System.Windows;
 
 namespace Galimsky_DayPlanner
 {
@@ -25,62 +7,9 @@ namespace Galimsky_DayPlanner
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        #region components def
-        private DaysRepo _repo;
-
-        #endregion
-
         public MainWindow()
         {
-            _repo = DaysRepo.Instance;
             InitializeComponent();
-            DataContext = _repo;
-        }
-        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
-        {
-            _repo.SelectedDate = mainCalendar.SelectedDate.GetValueOrDefault();
-        }
-
-        private void DayTasksList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            //if (e.AddedItems.Count <= 0)
-            //    _repo.DayTaskListSelection = null;
-            if (e.AddedItems.Count > 0)
-                _repo.DayTaskListSelection = e.AddedItems[0] as TaskData;
-        }
-
-        private void Tasks_EditItem_Click(object sender, RoutedEventArgs e)
-        {
-            if (_repo.DayTaskListSelection != null)
-            {
-                TaskEditWindow taskEditorWindow = new TaskEditWindow();
-                taskEditorWindow.ConfigureWindow(TaskEditorMode.Edit);
-                taskEditorWindow.Show();
-            }
-        }
-
-        private void SaveData_Click(object sender, RoutedEventArgs e)
-        {
-            App app = Application.Current as App;
-            app.SaveData();
-        }
-
-        private void Tasks_AddItem_Click(object sender, RoutedEventArgs e)
-        {
-            TaskEditWindow taskEditorWindow = new TaskEditWindow();
-            taskEditorWindow.ConfigureWindow(TaskEditorMode.New);
-            taskEditorWindow.Show();
-        }
-
-
-        private void PhonesBook_Click(object sender, RoutedEventArgs e)
-        {
-            PhonesWindow phonesWindow = new PhonesWindow();
-            phonesWindow.Show();
         }
     }
-
-
-
 }
---
+++ Galimsky DayPlanner/TasksControl.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Galimsky_DayPlanner
+{
+    /// <summary>
+    /// Interaction logic for TasksControl.xaml
+    /// </summary>
+    public partial class TasksControl : UserControl
+    {
+
+        #region components def
+        private DaysRepo _repo;
+
+        #endregion
+
+        public TasksControl()
+        {
+            _repo = DaysRepo.Instance;
+            InitializeComponent();
+            DataContext = _repo;
+        }
+
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _repo.SelectedDate = mainCalendar.SelectedDate.GetValueOrDefault();
+        }
+
+        private void DayTasksList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //if (e.AddedItems.Count <= 0)
+            //    _repo.DayTaskListSelection = null;
+            if (e.AddedItems.Count > 0)
+                _repo.DayTaskListSelection = e.AddedItems[0] as TaskData;
+        }
+        private void Tasks_EditItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (_repo.DayTaskListSelection != null)
+            {
+                TaskEditWindow taskEditorWindow = new TaskEditWindow();
+                taskEditorWindow.ConfigureWindow(TaskEditorMode.Edit);
+                taskEditorWindow.Show();
+            }
+        }
+
+        private void SaveData_Click(object sender, RoutedEventArgs e)
+        {
+            App app = Application.Current as App;
+            app.SaveData();
+        }
+
+        private void Tasks_AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            TaskEditWindow taskEditorWindow = new TaskEditWindow();
+            taskEditorWindow.ConfigureWindow(TaskEditorMode.New);
+            taskEditorWindow.Show();
+        }
+
+        private void PhonesBook_Click(object sender, RoutedEventArgs e)
+        {
+            PhonesWindow phonesWindow = new PhonesWindow();
+            phonesWindow.Show();
+        }
+    }
+}
